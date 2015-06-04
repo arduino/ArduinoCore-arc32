da2e44848abcb12f4f6c9a891a55425b330fdb25
davids <david.silva@emutex.com>
2015-06-04 15:20:53 +0100

    implement wiring_shift()

--- cores/arduino/wiring_shift.c
+++ cores/arduino/wiring_shift.c
@@ -1,24 +1,23 @@
 /*
-  wiring_shift.c - shiftOut() function
-  Part of Arduino - http://www.arduino.cc/
-  Copyright (c) 2005-2006 David A. Mellis
+  Arduino.h - Main include file for the Arduino SDK
+  Copyright (c) 2005-2013 Arduino Team.  All right reserved.
+
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
   License as published by the Free Software Foundation; either
   version 2.1 of the License, or (at your option) any later version.
+
   This library is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
   Lesser General Public License for more details.
-  You should have received a copy of the GNU Lesser General
-  Public License along with this library; if not, write to the
-  Free Software Foundation, Inc., 59 Temple Place, Suite 330,
-  Boston, MA  02111-1307  USA
-  $Id: wiring.c 248 2007-02-03 15:36:30Z mellis $
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
 #include "Arduino.h"
-#include "portable.h"
 #ifdef __cplusplus
  extern "C" {
 #endif
@@ -28,13 +27,12 @@ uint32_t shiftIn( uint32_t ulDataPin, uint32_t ulClockPin, uint32_t ulBitOrder )
 
 	uint32_t value = 0;
 	uint32_t i;
-	for (i = 0; i < 32 ; i++) {
-
+	for (i = 0; i < 8 ; i++) {
 		digitalWrite(ulClockPin, HIGH);
 		if (ulBitOrder == LSBFIRST)
 			value |= digitalRead(ulDataPin) << i;
 		else
-			value |= digitalRead(ulDataPin) << (32-i);
+			value |= digitalRead(ulDataPin) << (8-i);
 
 		digitalWrite(ulClockPin,LOW);
 	}
@@ -47,12 +45,11 @@ uint32_t shiftIn( uint32_t ulDataPin, uint32_t ulClockPin, uint32_t ulBitOrder )
 void shiftOut( uint32_t ulDataPin, uint32_t ulClockPin, uint32_t ulBitOrder, uint32_t ulVal ) {
 
 	int i;
-	for(i = 0; i < 32; i++) {
-	
+	for(i = 0; i < 8; i++) {
 		if(ulBitOrder == LSBFIRST)
 			digitalWrite(ulDataPin, !!(ulVal & (1 << i)));
 		else
-			digitalWrite(ulDataPin, !!(ulVal & (1 << (32-i))));
+			digitalWrite(ulDataPin, !!(ulVal & (1 << (8-i))));
 
 		digitalWrite(ulClockPin, HIGH);
 		digitalWrite(ulClockPin, LOW);
