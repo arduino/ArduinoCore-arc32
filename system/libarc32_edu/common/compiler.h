1cdc4f0688be19e32396d50e731ff3cf16b7d8a6
Bogdan Pricop <bogdan.pricop@emutex.com>
2015-06-30 15:27:49 +0100

    Update source code with WRS restrictive license headers
    * Delete arcv2_timer1.[c|h] driver - not used at this stage.
    * arcv2_timer0 driver: update license header to WRS permissive license;
      arcv2_timer0.h doesn't exist anymore in the latest Tiny Mountain source
      code, but we use it, stick in the same WRS permissive license header.
    * For linker.cmd and flsh.ld linker scripts: replace license header with
      the WRS permissive one fomr the original linker.cmd files from
      Tiny Mountain OS
    * aux_regs.h - derived work from aux_reg.h from Tiny Mountain OS;
      change license header to WRS permissive.
    * remove license header as in the original files from thunderdome code
      base: compiler.h, portable.h
    
    Signed-off-by: Bogdan Pricop <bogdan.pricop@emutex.com>

--- system/libarc32_edu/common/compiler.h
+++ system/libarc32_edu/common/compiler.h
@@ -1,13 +1,3 @@
-/* aux_regs.h - ARCv2 auxiliary registers definitions */
-
-/*
- * Copyright (c) 2014 Wind River Systems, Inc.
- *
- * The right to copy, distribute or otherwise make use of this software
- * may be licensed only pursuant to the terms of an applicable Wind River
- * license agreement.
- */
-
 #ifndef _COMPILER_H__
 #define _COMPILER_H__
 
@@ -17,7 +7,7 @@
 #define _Rarely(x) __builtin_expect(!!((x)), 0)
 #define _sr(_src_, _reg_) __builtin_arc_sr((unsigned int)_src_, _reg_)
 #define _lr(_reg_) __builtin_arc_lr(_reg_)
-#define _nop() 
+#define _nop()
 
 #endif
 
