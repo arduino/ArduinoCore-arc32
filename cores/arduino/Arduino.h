25acb322cad8505f9564ca435b0e113bf5b5d9df
Calvin Park <calvin.s.park@intel.com>
2015-11-25 16:28:00 -0800

    Fixed typo in old IDE version error message

--- cores/arduino/Arduino.h
+++ cores/arduino/Arduino.h
@@ -30,7 +30,7 @@
 //#include "itoa.h"
 
 #if ARDUINO < 10607
-#error IDE version incompatible with Arduino 101, please upgrade to it to version 1.6.7 or newer
+#error IDE version incompatible with Arduino 101. Please upgrade to version 1.6.7 or newer.
 #endif
 
 #ifdef __cplusplus
