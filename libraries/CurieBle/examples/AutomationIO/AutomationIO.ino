7eea01f3e26eda828208ee64068dde040b253e88
Sandeep Mistry <s.mistry@arduino.cc>
2015-11-27 13:22:31 -0500

    Add CurieBle.h for main lib header

--- libraries/CurieBle/examples/AutomationIO/AutomationIO.ino
+++ libraries/CurieBle/examples/AutomationIO/AutomationIO.ino
@@ -15,11 +15,7 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
-#include <BleCharacteristic.h>
-#include <BleDevice.h>
-#include <BleService.h>
-#include <BleCommon.h>
-#include <BleDescriptor.h>
+#include <CurieBle.h>
 
 /*
  * This sketch partially implements the standard Bluetooth Low-Energy "Automation IO" service.
