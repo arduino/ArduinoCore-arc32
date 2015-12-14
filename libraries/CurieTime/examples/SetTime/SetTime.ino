d0c2800286ecf575fa900d9210d7473c9e68ba04
Sandeep Mistry <s.mistry@arduino.cc>
2015-12-14 14:30:26 -0500

    Simplify SetTime example, use hard coded time

--- libraries/CurieTime/examples/SetTime/SetTime.ino
+++ libraries/CurieTime/examples/SetTime/SetTime.ino
@@ -1,32 +1,11 @@
 #include <CurieTime.h>
 
-const char *monthName[12] = {
-  "Jan", "Feb", "Mar", "Apr", "May", "Jun",
-  "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
-};
-
-int Hour, Min, Sec;
-int Day, Month, Year;
-
 void setup() {
   while (!Serial);
   Serial.begin(9600);
 
-  // get the date and time the compiler was run
-  if (getDate(__DATE__) && getTime(__TIME__)) {
-    Serial.print("Curie configured Time=");
-    Serial.print(__TIME__);
-    Serial.print(", Date=");
-    Serial.println(__DATE__);
-
-    setTime(Hour, Min, Sec, Day, Month, Year);
-  } else {
-    Serial.print("Could not parse info from the compiler, Time=\"");
-    Serial.print(__TIME__);
-    Serial.print("\", Date=\"");
-    Serial.print(__DATE__);
-    Serial.println("\"");
-  }
+  // set the current time to 14:27:00, December 14th, 2015
+  setTime(14, 27, 00, 14, 12, 2015);
 }
 
 void loop() {
@@ -49,79 +28,6 @@ void loop() {
   Serial.println();
 
   delay(1000);
-}
-
-bool getTime(const char *str)
-{
-  // Get time from string of format HH:MM:SS
-  String s = str;
-
-  int firstColonIndex = s.indexOf(":");
-  int lastColonIndex = s.lastIndexOf(":");
-
-  if (firstColonIndex == -1) {
-    // first colon not found
-    return false;
-  }
-
-  if (lastColonIndex == -1) {
-    // last colon not found
-    return false;
-  }
-
-  if (firstColonIndex == lastColonIndex) {
-    // only one colon, first and last index are the same
-    return false;
-  }
-
-  String hourString = s.substring(0, firstColonIndex);
-  String minuteString = s.substring(firstColonIndex + 1, lastColonIndex);
-  String secondString = s.substring(lastColonIndex + 1);
-
-  Hour = hourString.toInt();
-  Min = minuteString.toInt();
-  Sec = secondString.toInt();
-
-  return true;
-}
-
-bool getDate(const char *str)
-{
-  // Get Date from string of format MMM DD YYYY
-  String s = str;
-
-  int firstSpaceIndex = s.indexOf(" ");
-  int lastSpaceIndex = s.lastIndexOf(" ");
-
-  if (firstSpaceIndex == -1) {
-    // first space not found
-    return false;
-  }
-
-  if (lastSpaceIndex == -1) {
-    // last space not found
-    return false;
-  }
-
-  if (firstSpaceIndex == lastSpaceIndex) {
-    // only one space, first and last index are the same
-    return false;
-  }
-
-  String monthString = s.substring(0, firstSpaceIndex);
-  String dayString = s.substring(firstSpaceIndex + 1, lastSpaceIndex);
-  String yearString = s.substring(lastSpaceIndex + 1);
-
-  for (Month = 1; Month <= 12; Month++) {
-    if (monthString.equals(monthName[Month - 1])) {
-      break;
-    }
-  }
-
-  Day = dayString.toInt();
-  Year = yearString.toInt();
-
-  return (Month <= 12);
 }
 
 void print2digits(int number) {
