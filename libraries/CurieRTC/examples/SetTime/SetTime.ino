b872431e1df13cebb07cef8b2d85bc0906202462
Sandeep Mistry <s.mistry@arduino.cc>
2015-12-14 11:48:01 -0500

    Parse date and time strings using Arduino string class instead of sscanf

--- libraries/CurieRTC/examples/SetTime/SetTime.ino
+++ libraries/CurieRTC/examples/SetTime/SetTime.ino
@@ -9,9 +9,9 @@ int Hour, Min, Sec;
 int Day, Month, Year;
 
 void setup() {
-  while(!Serial);
+  while (!Serial);
   Serial.begin(9600);
-  
+
   // get the date and time the compiler was run
   if (getDate(__DATE__) && getTime(__TIME__)) {
     Serial.print("Curie configured Time=");
@@ -53,23 +53,73 @@ void loop() {
 
 bool getTime(const char *str)
 {
-  if (sscanf(str, "%d:%d:%d", &Hour, &Min, &Sec) != 3) return false;
+  // Get time from string of format HH:MM:SS
+  String s = str;
+
+  int firstColonIndex = s.indexOf(":");
+  int lastColonIndex = s.lastIndexOf(":");
+
+  if (firstColonIndex == -1) {
+    // first colon not found
+    return false;
+  }
+
+  if (lastColonIndex == -1) {
+    // last colon not found
+    return false;
+  }
+
+  if (firstColonIndex == lastColonIndex) {
+    // only one colon, first and last index are the same
+    return false;
+  }
+
+  String hourString = s.substring(0, firstColonIndex);
+  String minuteString = s.substring(firstColonIndex + 1, lastColonIndex);
+  String secondString = s.substring(lastColonIndex + 1);
+
+  Hour = hourString.toInt();
+  Min = minuteString.toInt();
+  Sec = secondString.toInt();
+
   return true;
 }
 
 bool getDate(const char *str)
 {
-  char monthString[12];
+  // Get Date from string of format MMM DD YYYY
+  String s = str;
 
-  if (sscanf(str, "%s %d %d", monthString, &Day, &Year) != 3) {
+  int firstSpaceIndex = s.indexOf(" ");
+  int lastSpaceIndex = s.lastIndexOf(" ");
+
+  if (firstSpaceIndex == -1) {
+    // first space not found
     return false;
   }
 
+  if (lastSpaceIndex == -1) {
+    // last space not found
+    return false;
+  }
+
+  if (firstSpaceIndex == lastSpaceIndex) {
+    // only one space, first and last index are the same
+    return false;
+  }
+
+  String monthString = s.substring(0, firstSpaceIndex);
+  String dayString = s.substring(firstSpaceIndex + 1, lastSpaceIndex);
+  String yearString = s.substring(lastSpaceIndex + 1);
+
   for (Month = 1; Month <= 12; Month++) {
-    if (strcmp(monthString, monthName[Month - 1]) == 0) {
+    if (monthString.equals(monthName[Month - 1])) {
       break;
     }
   }
+
+  Day = dayString.toInt();
+  Year = yearString.toInt();
 
   return (Month <= 12);
 }
