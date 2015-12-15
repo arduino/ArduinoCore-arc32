46f53922b2c46b164bcd8e0f6c44999d5b936e41
Sidney Leung <sidney.leung@intel.com>
2015-12-15 11:12:47 -0800

    Use timer-1 of the Curie Arc in place of the TimerOne library for the AVR platform.
    Code modifications:
    
    1.  Use boolean to indicate toggling of output pwm signal instead of an integer.
    2.  Create an addition pwmStart method that takes a discrete number range for the duration of the duty cycle.
    
    3.  Changed the library name from CurieArcTimerOne to CurieTimerOne, as per our discussion with Arduino folks.
    
    4.  Replaced the refer to CurieArcTimer with CurieTimer for all files.

---
+++ libraries/CurieTimerOne/examples/CurieTimer1PWM/CurieTimer1PWM.ino
@@ -0,0 +1,20 @@
+#include "CurieTimer.h"
+
+void setup() {
+  // Setup a PWM signal on pin 13, onboard LED, with a 25% duty cycle
+  // of a 1 second period (1000000 usec), as follow (please note the
+  // decimal point to indicate double):
+
+  CurieTimerOne.pwmStart(13, 25.0, 1000000);
+
+  // Or, use discrete number range, 0-1023, to define the duty period,
+  // 255 is 24.9%, as follow:
+
+  // CurieTimerOne.pwmStart(13, 255, 1000000);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  delay( 10000 );
+}
