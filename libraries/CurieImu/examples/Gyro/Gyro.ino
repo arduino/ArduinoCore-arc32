7e628f15227d24c81c4f42bfe1f3cb351bdb8f78
Sandeep Mistry <s.mistry@arduino.cc>
2015-12-21 11:12:16 -0500

    Initial gyroscope example

---
+++ libraries/CurieImu/examples/Gyro/Gyro.ino
@@ -0,0 +1,84 @@
+/*
+   Copyright (c) 2015 Intel Corporation.  All rights reserved.
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+*/
+
+/*
+   This sketch example demonstrates how the BMI160 on the
+   Intel(R) Curie(TM) module can be used to read gyroscope data
+*/
+
+#include "CurieImu.h"
+
+void setup() {
+  Serial.begin(9600); // initialize Serial communication
+  while (!Serial);    // wait for the serial port to open
+
+  // initialize device
+  Serial.println("Initializing IMU device...");
+  CurieIMU.begin();
+
+  // Set the accelerometer range to 250 degrees/second
+  CurieIMU.setGyroRange(CURIE_IMU_GYRO_RANGE_250);
+
+  Serial.println("About to calibrate gyro. Make sure your board is stable and upright");
+  delay(5000);
+
+  // start gyro
+  Serial.print("Starting gyro calibration...");
+  CurieIMU.autoCalibrateGyroOffset();
+  Serial.println(" Done");
+
+  Serial.println("Enabling gyro offset compensation");
+  CurieIMU.enableGyroOffset(true);
+}
+
+void loop() {
+  short int gxRaw, gyRaw, gzRaw;         // raw gyro values
+  float gx, gy, gz;
+
+  // read raw gyro measurements from device
+  CurieIMU.readGyro(gxRaw, gyRaw, gzRaw);
+
+  // convert the raw gyro data to degrees/second
+  gx = convertRawGyro(gxRaw);
+  gy = convertRawGyro(gyRaw);
+  gz = convertRawGyro(gzRaw);
+
+  // display tab-separated gyro x/y/z values
+  Serial.print("g:\t");
+  Serial.print(gx);
+  Serial.print("\t");
+  Serial.print(gy);
+  Serial.print("\t");
+  Serial.print(gz);
+  Serial.println();
+
+  // wait 5 seconds
+  delay(5000);
+}
+
+float convertRawGyro(short gRaw) {
+  // since we are using 250 degrees/seconds range
+  // -250 maps to a raw value of -32768
+  // +250 maps to a raw value of 32767
+  
+  float g = (gRaw * 250.0) / 32768.0;
+
+  return g;
+}
+
