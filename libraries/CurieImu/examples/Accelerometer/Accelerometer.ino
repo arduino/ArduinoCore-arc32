a50c6b649f093f6d5dbb19e9e376f700976b833e
Sandeep Mistry <s.mistry@arduino.cc>
2015-12-21 11:12:32 -0500

    Update example header

--- libraries/CurieImu/examples/Accelerometer/Accelerometer.ino
+++ libraries/CurieImu/examples/Accelerometer/Accelerometer.ino
@@ -1,33 +1,25 @@
 /*
-  ===============================================
-  Example sketch for CurieIMU library for Intel(R) Curie(TM) devices.
-  Copyright (c) 2015 Intel Corporation.  All rights reserved.
+   Copyright (c) 2015 Intel Corporation.  All rights reserved.
 
-  Based on I2C device class (I2Cdev) demonstration Arduino sketch for MPU6050
-  class by Jeff Rowberg: https://github.com/jrowberg/i2cdevlib
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
 
-  ===============================================
-  I2Cdev device library code is placed under the MIT license
-  Copyright (c) 2011 Jeff Rowberg
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
 
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files (the "Software"), to deal
-  in the Software without restriction, including without limitation the rights
-  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-  copies of the Software, and to permit persons to whom the Software is
-  furnished to do so, subject to the following conditions:
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
-  The above copyright notice and this permission notice shall be included in
-  all copies or substantial portions of the Software.
+*/
 
-  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-  THE SOFTWARE.
-  ===============================================
+/*
+   This sketch example demonstrates how the BMI160 on the
+   Intel(R) Curie(TM) module can be used to read accelerometer data
 */
 
 #include "CurieImu.h"
