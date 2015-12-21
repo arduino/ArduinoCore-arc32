cfd445144984d9aaaf3995f257cc5db0e14f04af
Sandeep Mistry <s.mistry@arduino.cc>
2015-12-21 10:57:14 -0500

    Correct typo in example, az was printed instead of ax

--- libraries/CurieImu/examples/RawImuDataSerial/RawImuDataSerial.ino
+++ libraries/CurieImu/examples/RawImuDataSerial/RawImuDataSerial.ino
@@ -137,7 +137,7 @@ void loop() {
 
   // display tab-separated accel/gyro x/y/z values
   Serial.print("a/g:\t");
-  Serial.print(az);
+  Serial.print(ax);
   Serial.print("\t");
   Serial.print(ay);
   Serial.print("\t");
