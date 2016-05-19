92818058e83302e478e7cdddd60d1d5568ce9bc5
erik.nyquist <enyquist@geronimo.amr.corp.intel.com>
2016-05-19 10:34:30 -0700

    Add Paul Stoffregen's SerialFlash library

---
+++ libraries/SerialFlash/examples/FileWrite/FileWrite.ino
@@ -0,0 +1,50 @@
+//SerialFlash library API: https://github.com/PaulStoffregen/SerialFlash
+
+#include <SerialFlash.h>
+#include <SPI.h>
+
+#define FSIZE 256
+
+const char *filename = "myfile.txt";
+const char *contents = "0123456789ABCDEF";
+
+const int FlashChipSelect = 21; // digital pin for flash chip CS pin
+
+void setup() {
+  Serial.begin(9600);
+
+  // wait for Arduino Serial Monitor
+  while (!Serial) ;
+  delay(100);
+
+  // Init. SPI Flash chip
+  if (!SerialFlash.begin(FlashChipSelect)) {
+    Serial.println("Unable to access SPI Flash chip");
+  }
+
+  SerialFlashFile file;
+
+  // Create the file if it doesn't exist
+  if (!create_if_not_exists(filename)) {
+    Serial.println("Not enough space to create file " + String(filename));
+    return;
+  }
+
+  // Open the file and write test data
+  file = SerialFlash.open(filename);
+  file.write(contents, strlen(contents) + 1);
+  Serial.println("String \"" + String(contents) + "\" written to file " + String(filename));
+}
+
+bool create_if_not_exists (const char *filename) {
+  if (!SerialFlash.exists(filename)) {
+    Serial.println("Creating file " + String(filename));
+    return SerialFlash.create(filename, FSIZE);
+  }
+
+  Serial.println("File " + String(filename) + " already exists");
+  return true;
+}
+
+void loop() {
+}
---
+++ libraries/SerialFlash/examples/ListFiles/ListFiles.ino
@@ -0,0 +1,63 @@
+// SerialFlash library API: https://github.com/PaulStoffregen/SerialFlash
+
+#include <SerialFlash.h>
+#include <SPI.h>
+
+const int FlashChipSelect = 21; // digital pin for flash chip CS pin
+
+void setup() {
+  //uncomment these if using Teensy audio shield
+  //SPI.setSCK(14);  // Audio shield has SCK on pin 14
+  //SPI.setMOSI(7);  // Audio shield has MOSI on pin 7
+
+  //uncomment these if you have other SPI chips connected
+  //to keep them disabled while using only SerialFlash
+  //pinMode(4, INPUT_PULLUP);
+  //pinMode(10, INPUT_PULLUP);
+
+  Serial.begin(9600);
+
+  // wait for Arduino Serial Monitor
+  while (!Serial) ;
+  delay(100);
+  Serial.println("All Files on SPI Flash chip:");
+
+  if (!SerialFlash.begin(FlashChipSelect)) {
+    error("Unable to access SPI Flash chip");
+  }
+
+  SerialFlash.opendir();
+  unsigned int count = 0;
+  while (1) {
+    char filename[64];
+    unsigned long filesize;
+
+    if (SerialFlash.readdir(filename, sizeof(filename), filesize)) {
+      Serial.print("  ");
+      Serial.print(filename);
+      spaces(20 - strlen(filename));
+      Serial.print("  ");
+      Serial.print(filesize);
+      Serial.print(" bytes");
+      Serial.println();
+    } else {
+      break; // no more files
+    }
+  }
+}
+
+void spaces(int num) {
+  for (int i=0; i < num; i++) {
+    Serial.print(" ");
+  }
+}
+
+void loop() {
+}
+
+void error(const char *message) {
+  while (1) {
+    Serial.println(message);
+    delay(2500);
+  }
+}
