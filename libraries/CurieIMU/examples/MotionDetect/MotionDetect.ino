baa4cb47727fedccbc25e78572fe2f20add0b8af
caihongm <caihong.ma@intel.com>
2016-05-27 02:22:09 +0800

    Jira509:add IMU examples:FreeFallDetection, MotionDetection,ZeroMotionDetection; change CurieIMU.cpp line490 from 'setZeroMotionDetectionThreshold' to 'setZeroMtionDetectionDuration'

---
+++ libraries/CurieIMU/examples/MotionDetect/MotionDetect.ino
@@ -0,0 +1,73 @@
+/*
+   Copyright (c) 2015 Intel Corporation.  All rights reserved.
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+*/
+
+/*
+   This sketch example demonstrates how the BMI160 accelerometer on the
+   Intel(R) Curie(TM) module can be used to detect motion events
+*/
+
+#include "CurieIMU.h"
+
+boolean blinkState = false;          // state of the LED
+unsigned long loopTime = 0;          // get the time since program started
+unsigned long interruptsTime = 0;    // get the time when motion event is detected
+
+void setup() {
+  Serial.begin(9600);
+
+  /* Initialise the IMU */
+  CurieIMU.begin();
+  CurieIMU.attachInterrupt(eventCallback);
+
+  /* Enable Motion Detection */
+  CurieIMU.setDetectionThreshold(CURIE_IMU_MOTION, 20); // 20mg
+  CurieIMU.setDetectionDuration(CURIE_IMU_MOTION, 10);  // trigger times of consecutive slope data points
+  CurieIMU.interrupts(CURIE_IMU_MOTION);
+
+  Serial.println("IMU initialisation complete, waiting for events...");
+}
+
+void loop() {
+  // if motion is detected in 1000ms, LED will be turned up
+  loopTime = millis();
+  if(abs(loopTime -interruptsTime) < 1000 )
+    blinkState = true;
+  else
+    blinkState = false;
+  digitalWrite(13, blinkState);
+}
+
+
+static void eventCallback(void){
+  if (CurieIMU.getInterruptStatus(CURIE_IMU_MOTION)) {
+    if (CurieIMU.motionDetected(X_AXIS, POSITIVE))
+      Serial.println("Negative motion detected on X-axis");
+    if (CurieIMU.motionDetected(X_AXIS, NEGATIVE))
+      Serial.println("Positive motion detected on X-axis");
+    if (CurieIMU.motionDetected(Y_AXIS, POSITIVE))
+      Serial.println("Negative motion detected on Y-axis");
+    if (CurieIMU.motionDetected(Y_AXIS, NEGATIVE))
+      Serial.println("Positive motion detected on Y-axis");
+    if (CurieIMU.motionDetected(Z_AXIS, POSITIVE))
+      Serial.println("Negative motion detected on Z-axis");
+    if (CurieIMU.motionDetected(Z_AXIS, NEGATIVE))
+      Serial.println("Positive motion detected on Z-axis");
+    interruptsTime = millis(); 
+  } 
+}
---
+++ libraries/CurieIMU/examples/ZeroMotionDetect/ZeroMotionDetect.ino
@@ -0,0 +1,59 @@
+/*
+   Copyright (c) 2015 Intel Corporation.  All rights reserved.
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+*/
+
+/*
+   This sketch example demonstrates how the BMI160 accelerometer on the
+   Intel(R) Curie(TM) module can be used to detect zero motion events
+*/
+#include "CurieIMU.h"
+
+boolean blinkState = false;          // state of the LED
+unsigned long loopTime = 0;          // get the time since program started
+unsigned long interruptsTime = 0;    // get the time when zero motion event is detected
+void setup() {
+  Serial.begin(9600);
+
+  /* Initialise the IMU */
+  blinkState = CurieIMU.begin();
+  CurieIMU.attachInterrupt(eventCallback);
+
+  /* Enable Zero Motion Detection */
+  CurieIMU.setDetectionThreshold(CURIE_IMU_ZERO_MOTION, 1500); // 1.5g=1500mg
+  CurieIMU.setDetectionDuration(CURIE_IMU_ZERO_MOTION, 25);    // 25s
+  CurieIMU.interrupts(CURIE_IMU_ZERO_MOTION);
+
+  Serial.println("IMU initialisation complete, waiting for events...");
+}
+
+void loop() {
+  //if zero motion is detected in 1500ms, LED will be turned up
+  loopTime = millis();
+  if(abs(loopTime -interruptsTime) < 1500)     
+    blinkState = true;
+  else
+    blinkState = false;
+  digitalWrite(13, blinkState);
+}
+
+static void eventCallback(void){
+  if (CurieIMU.getInterruptStatus(CURIE_IMU_ZERO_MOTION)) {
+    interruptsTime = millis(); 
+    Serial.println("zero motion detected...");
+  }  
+}
