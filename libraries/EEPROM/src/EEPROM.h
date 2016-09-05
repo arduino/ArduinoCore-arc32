8443b03bae82ab3578c8c240320835fac35b247f
Martino Facchin <m.facchin@arduino.cc>
2016-09-05 15:53:20 +0200

    Fix EEPROM size()

--- libraries/EEPROM/src/EEPROM.h
+++ libraries/EEPROM/src/EEPROM.h
@@ -234,7 +234,7 @@ struct EEPROMClass{
     //STL and C++11 iteration capability.
     EEPtr begin()                        { return 0x00; }
     EEPtr end()                          { return length(); } //Standards requires this to be the item after the last valid entry. The returned pointer is invalid.
-    uint16_t length()                    { return E2END + 1; }
+    uint16_t length()                    { return EEPROM_SIZE; }
     
     //Functionality to 'get' and 'put' objects to and from EEPROM.
     template< typename T > T &get( int idx, T &t ){
