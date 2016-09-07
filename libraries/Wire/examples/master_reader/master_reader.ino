d5b555385e846364472ac57aaae31c543fa3a152
Biagio Montaruli <biagio.hkr@gmail.com>
2016-09-07 13:10:49 +0200

    Fix for USB virtual serial port in sketches of Wire library
    Since Arduino/Genuino 101 uses USB native port, wait for the Serial port
    to open before executing the next lines of code to not lose serial data
    already sent to the Serial monitor
    
    Signed-off-by: Biagio Montaruli <biagio.hkr@gmail.com>

--- libraries/Wire/examples/master_reader/master_reader.ino
+++ libraries/Wire/examples/master_reader/master_reader.ino
@@ -15,7 +15,8 @@
 void setup()
 {
   Wire.begin();        // join i2c bus (address optional for master)
-  Serial.begin(9600);  // start serial for output
+  Serial.begin(9600);  // initialize Serial communication
+  while(!Serial) ;     // wait for serial port to connect.
 }
 
 void loop()
