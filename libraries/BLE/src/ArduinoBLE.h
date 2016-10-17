5420e4cd0d2666a4deeee674f0db5c8640c3894b
lianggao <liang.gao@intel.com>
2016-10-17 16:11:18 +0800

    Arduino BLE new library init version
    Know issues:
    1. Memory issue. Some times the memcmp not work correct.
    2. Central not complete. But can do scan.

---
+++ libraries/BLE/src/ArduinoBLE.h
@@ -0,0 +1,42 @@
+/*
+  BLE API
+  Copyright (c) 2016 Arduino LLC. All right reserved.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+*/
+
+#ifndef ARDUINO_BLE_H
+#define ARDUINO_BLE_H
+
+#define ARDUINO_BLE_API_VERSION 10000 // version 1.0.0
+
+class BLEDevice;
+class BLECharacteristic;
+class BLEDescriptor;
+class BLEService;
+class BLECharacteristicImp;
+
+#include "BLECommon.h"
+
+#include "BLEDevice.h"
+
+#include "BLECharacteristic.h"
+#include "BLEDescriptor.h"
+#include "BLEService.h"
+
+
+extern BLEDevice BLE;
+
+#endif
